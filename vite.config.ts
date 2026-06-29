f02defaed5d48548e1abdfd5b5082f28692defc2
guelfis <serena.guelfi@libero.it>
2026-06-29 20:23:14 +0200

    make the app as PWA web app

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,37 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'script',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Laundry Planner',
+        short_name: 'LaundryApp',
+        theme_color: '#121212',       // System status bar color defaults
+        background_color: '#121212',  // Splash screen background color
+        display: 'standalone',
+        orientation: 'portrait',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
